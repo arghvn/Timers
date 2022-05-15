3272d720ab8d6aed69a01bdaec32a448ed4cdb12
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-15 22:39:32 +0430

    comments

--- timers.go
+++ timers.go
@@ -1,5 +1,9 @@
 package main
 
+//We often want to execute Go code at some point in the future,
+// or repeatedly at some interval. Go’s built-in timer and ticker features make both of these tasks easy.
+// We’ll look first at timers and then at tickers.
+
 import (
     "fmt"
     "time"
@@ -9,18 +13,44 @@ func main() {
 
     timer1 := time.NewTimer(2 * time.Second)
 
+	//Timers represent a single event in the future.
+	// You tell the timer how long you want to wait, and it provides a
+	// channel that will be notified at that time. 
+	//This timer will wait 2 seconds.
+
     <-timer1.C
+
+	//The <-timer1.C blocks on the timer’s channel C until it sends a value indicating that the timer fired.
+
     fmt.Println("Timer 1 fired")
 
     timer2 := time.NewTimer(time.Second)
+
+	// One reason a timer may be useful is that you can cancel the timer before it fires. Here’s an example of that.
+
     go func() {
+
         <-timer2.C
+
+		//Give the timer2 enough time to fire, if it ever was going to, to show it is in fact stopped.
+
         fmt.Println("Timer 2 fired")
+
     }()
+
     stop2 := timer2.Stop()
+
     if stop2 {
+
         fmt.Println("Timer 2 stopped")
     }
 
     time.Sleep(2 * time.Second)
+
 }
+
+// The first timer will fire ~2s after we start the program, but the second should be stopped .
+
+//output :
+//Timer 1 fired
+//Timer 2 stopped
